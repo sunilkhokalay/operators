31979fcdd2574cdf887fc116a4cbcd712a260f39
Sunil Kumar K <sunil.kumar_k@nokia.com>
2021-05-16 13:31:32 +0000

    Updates controllers.go

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,22 +4,36 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-var (
-	PodConciler       *PodReconciler
-	ConfigMapConciler *ConfigMapReconciler
-)
+func AddToManager(mgr ctrl.Manager, concilers ...interface{}) {
 
-func AddToManager(mgr ctrl.Manager) {
-	PodConciler = &PodReconciler{Scheme: mgr.GetScheme(), Client: mgr.GetClient()}
-	err := PodConciler.SetupWithManager(mgr)
-	if err != nil {
-		panic(err.Error())
+	for _, conciler := range concilers {
+		switch conciler.(type) {
+		case *PodReconciler:
+			conciler.(*PodReconciler).Scheme = mgr.GetScheme()
+			conciler.(*PodReconciler).Client = mgr.GetClient()
+			err := conciler.(*PodReconciler).SetupWithManager(mgr)
+			if err != nil {
+				panic(err.Error())
+			}
+
+		case *ConfigMapReconciler:
+			conciler.(*ConfigMapReconciler).Scheme = mgr.GetScheme()
+			conciler.(*ConfigMapReconciler).Client = mgr.GetClient()
+			err := conciler.(*ConfigMapReconciler).SetupWithManager(mgr)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+
 	}
-
-	ConfigMapConciler = &ConfigMapReconciler{Scheme: mgr.GetScheme(), Client: mgr.GetClient()}
-
-	err = ConfigMapConciler.SetupWithManager(mgr)
-	if err != nil {
-		panic(err.Error())
-	}
+	//
+	//PodConciler = &PodReconciler{Scheme: mgr.GetScheme(), Client: mgr.GetClient()}
+	//err := PodConciler.SetupWithManager(mgr)
+	//
+	//ConfigMapConciler = &ConfigMapReconciler{Scheme: mgr.GetScheme(), Client: mgr.GetClient()}
+	//
+	//err = ConfigMapConciler.SetupWithManager(mgr)
+	//if err != nil {
+	//	panic(err.Error())
+	//}
 }
